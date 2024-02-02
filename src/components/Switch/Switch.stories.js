b42d15453de58abb7ea8dd376cd861d8a7f2ef73
Parag Nair <parag.nair@onalytica.com>
2024-02-02 19:18:19 +0000

    Added stories for switch

---
+++ src/components/Switch/Switch.stories.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Switch from "./Switch";
+
+export default {
+    title: "Components/Switch",
+    component: Switch,
+    argTypes: {
+        label: {
+            control: "text",
+        },
+        isOn: {
+            control: "boolean",
+        },
+    },
+};
+
+const Template = (args) => {
+    return (
+        <div style={{width: `200px`}}>
+            <Switch {...args} />
+        </div>
+    );
+}
+
+export const Default = Template.bind({});
+Default.args = {
+    label: "Switch",
+    isOn: false,
+};
+
+export const On = Template.bind({});
+On.args = {
+    ...Default.args,
+    label: "Switch On",
+    isOn: true,
+};
+
+export const Off = Template.bind({});
+Off.args = {
+    ...Default.args,
+    label: "Switch Off",
+    isOn: false,
+};
